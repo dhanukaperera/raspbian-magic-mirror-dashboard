64ff0106ab409d6774c38b1c9536549c21a923f4
dhanukaperera <dhanukashehanperera@gmail.com>
2017-08-17 23:46:31 +0530

    add time 12hrs 24hours formater

--- src/app/components/clock/clock.component.ts
+++ src/app/components/clock/clock.component.ts
@@ -7,31 +7,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClockComponent implements OnInit {
 
-  private date:Date;
-  private hh:any;
-  private mm:any;
-  private ss:any;
+  // --Creating variables 
+  private date:Date;        // Use to hold the system current date.
+  private hh:any;           // Get the hour from the current date.
+  private mm:any;           // Get the mintues from the current date.
+  private ss:any;           // Get the seconds from the current date.
+  private period:String     // Time is AM or PM
+
+
+  //--
+
 
   constructor() { 
-    this.startTime();
+   
+   
   }
 
   ngOnInit() {
-    //this.startTime();
+    this.startTime();
+    
   }
 
   startTime():void{
+    // Method calls every 1s. Get the current time values and format it.
     setInterval(()=>{
       this.date = new Date();
       this.hh = this.formatTime(this.date.getHours()) ;
       this.mm = this.formatTime(this.date.getMinutes()) ;
       this.ss = this.formatTime(this.date.getSeconds()) ;
-    },1000)
+      
+      this.setTimeFormat(12);
+
+    },1000) // <-- 1000  miliseconds = 1 second
   }
   
+  // -- Add a zeor(0) to infront of a number if it's less than 10. This is used to format the date.
   formatTime(i):any{
      (i < 10) ? i= "0"+i : i=i; 
      return i;
+  }
+
+  //-- Change the time format 12hrs/24hrs
+  setTimeFormat(f):void{
+    // Show AM if the hours less than 12. Show PM if the hours grater than 12.
+    (this.hh < 12) ? this.period = "AM" : this.period = "PM";
+    // Subtract 12 for 12 hours time formate, else hide period from 24 hours format.
+    (f== 12) ? this.hh = parseInt(this.hh) - 12 : this.period = null;
+    
   }
 
 }
