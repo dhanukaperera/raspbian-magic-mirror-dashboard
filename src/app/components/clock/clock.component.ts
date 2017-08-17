7730f1828f4e1d6924d3152e9caf659224da22d6
dhanukaperera <dhanukashehanperera@gmail.com>
2017-08-17 08:51:46 +0530

    clock is ticking

--- src/app/components/clock/clock.component.ts
+++ src/app/components/clock/clock.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClockComponent implements OnInit {
 
-  constructor() { }
+  private date:Date;
+  private hh:any;
+  private mm:any;
+  private ss:any;
+
+  constructor() { 
+    this.startTime();
+  }
 
   ngOnInit() {
+    //this.startTime();
+  }
+
+  startTime():void{
+    setInterval(()=>{
+      this.date = new Date();
+      this.hh = this.formatTime(this.date.getHours()) ;
+      this.mm = this.formatTime(this.date.getMinutes()) ;
+      this.ss = this.formatTime(this.date.getSeconds()) ;
+    },1000)
+  }
+  
+  formatTime(i):any{
+     (i < 10) ? i= "0"+i : i=i; 
+     return i;
   }
 
 }
