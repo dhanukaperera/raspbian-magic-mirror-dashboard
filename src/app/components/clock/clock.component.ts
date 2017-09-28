e07bf80b173c02bd3f1945f318f1869c607b761d
RPA BOT10 <rpa10@nations.int>
2017-09-28 13:41:13 +0530

    clock module complete.

--- src/app/components/clock/clock.component.ts
+++ src/app/components/clock/clock.component.ts
@@ -21,6 +21,7 @@ export class ClockComponent implements OnInit {
 
   private display:any;
   private timeFormat:any
+  private displaySeconds:any;
   
   //--
 
@@ -29,12 +30,14 @@ export class ClockComponent implements OnInit {
     this.clock = db.object('/db/clock/', { preserveSnapshot: true })
     this.clock .subscribe(snapshot => {
 
-    /*  console.log(snapshot.key)
+    /*console.log(snapshot.key)
      console.log(snapshot.val().display)
      console.log(snapshot.val().timeformat) */
 
      this.display = snapshot.val().display;
      this.timeFormat = snapshot.val().timeformat;
+     this.displaySeconds =  snapshot.val().displayseconds;
+       
    });
    
 
@@ -43,18 +46,21 @@ export class ClockComponent implements OnInit {
 
   ngOnInit() {
     this.startTime();
+ 
   }
 
   startTime():void{
     // Method calls every 1s. Get the current time values and format it.
     setInterval(()=>{
       this.date = new Date();
-      
+  
       this.hh = this.formatTime(this.date.getHours()) ;
       this.mm = this.formatTime(this.date.getMinutes()) ;
       this.ss = this.formatTime(this.date.getSeconds()) ;
       
       this.setTimeFormat(this.timeFormat);
+      this.showSeconds(this.displaySeconds);
+      
 
     },1000) // <-- 1000  miliseconds = 1 second
   }
@@ -68,12 +74,26 @@ export class ClockComponent implements OnInit {
   //-- Change the time format 12hrs/24hrs
   setTimeFormat(f):void{
     // Show AM if the hours less than 12. Show PM if the hours grater than 12.
-    (this.hh < 12) ? this.period = "AM" : this.period = "PM";
     // Subtract 12 for 12 hours time formate, else hide period from 24 hours format.
-    (f== 12) ? this.hh = parseInt(this.hh) - 12 : this.period = null;
-    
+
+
+    if(f == 12){
+      (this.hh < 12) ? this.period = "AM" : this.period = "PM";
+      if(this.hh > 12) {
+        this.hh = this.hh-12;
+        this.hh = this.formatTime(this.hh);
+      }
+    }else if (f==24){
+      this.period = null;
+      
+    }
   }
 
+  showSeconds(option):void{
+    if(option != true){
+      this.ss = null;
+    }
+  }
 }
 
 
