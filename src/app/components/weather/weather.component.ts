ae0223b1bb95db4a8732fb03ffd760c1a1529c35
dhanukaperera <dhanukashehanperera@gmail.com>
2017-09-29 08:41:24 +0530

    quick push

--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -8,24 +8,24 @@ import DarkSkyApi from 'dark-sky-api';
   styleUrls: ['./weather.component.css']
 })
 export class WeatherComponent implements OnInit {
-  //exporting at 
+  // exporting at
 
-  private apiKey:string;
+  private apiKey: string;
   /* private latitude:string;
   private longitude:string; */
 
-  private temperature:string;
-  private ctiy:String;
-  private weather:String
+  private temperature: any;
+  private ctiy: String;
+  private weather: String;
 
-  private weatherData:any;
+  private weatherData: any;
 
-  constructor() { 
-    this.apiKey = "ec5efcbb0edafcbf140a73289ea588ff";
+  constructor() {
+    this.apiKey = 'ec5efcbb0edafcbf140a73289ea588ff';
   /*   this.longitude="6.9497";
     this.latitude = "80.7891"; */
-    this.ctiy = "Kollupitiya";
-  
+    this.ctiy = 'Kollupitiya';
+
     DarkSkyApi.apiKey = this.apiKey;
 
     this.getWeatherData();
@@ -33,22 +33,34 @@ export class WeatherComponent implements OnInit {
   }
 
   ngOnInit() {
-   
+
   }
 
-  
-  getWeatherData():void{
+
+  getWeatherData(): void {
  /*    const position = {
-      latitude: this.latitude, 
+      latitude: this.latitude,
       longitude: this.latitude
     }; */
 
+    // AIzaSyCXRPUXgm8x5N0BjKcas1zCyL6nzAb9YFY - Google API KEY
+// https://maps.googleapis.com/maps/api/geocode/json?latlng=7.1216460999999995,79.87920249999999&key=AIzaSyCXRPUXgm8x5N0BjKcas1zCyL6nzAb9YFY
+
     DarkSkyApi.loadCurrent().then(result => {
-      console.log(result)
+      console.log(result);
       this.weatherData = result;
 
-      this.temperature = this.weatherData.temperature;
+      this.temperature = parseFloat( this.weatherData.temperature).toFixed(0);
       this.weather = this.weatherData.summary;
+
+
+      let position;
+      DarkSkyApi.loadPosition()
+        .then(pos => {
+          position = pos;
+          console.log(position);
+        });
+
     });
 
   }
