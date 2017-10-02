129e9405cda2659e8e1a08c0faa9a05045fd1179
RPA BOT10 <rpa10@nations.int>
2017-10-02 18:24:08 +0530

    news feed update on intervals after reload

--- src/app/components/newsfeed/newsfeed.component.ts
+++ src/app/components/newsfeed/newsfeed.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  AngularFireDatabase,
+  FirebaseObjectObservable
+} from 'angularfire2/database';
 
 import {
   Http,
@@ -27,28 +31,56 @@ export class NewsfeedComponent implements OnInit {
 
   private newsArray: any;
 
-  constructor(private http: Http) {
+  private complimentsData: any;
 
-    this.NEWS_API_KEY = '2eaff1b640e6496a97d4791860fffa6b';
+  private interval: number;
+
+  constructor(private http: Http,db: AngularFireDatabase) {
     this.source = 'bbc-news';
     this.sortBy = 'top';
-    this.API_URL = `https://newsapi.org/v1/articles?source=${this.source}&sortBy=${this.sortBy}&apiKey=${this.NEWS_API_KEY}`;
+    this.NEWS_API_KEY = '2eaff1b640e6496a97d4791860fffa6b';
+    //this.interval = 3000;
 
-    console.log(this.API_URL);
-    this.getNewsFeed();
+    this.complimentsData =db.object('/db/newsfeed/',{
+      preserveSnapshot: true
+    });
+
+    this.complimentsData.subscribe(snapshot => {
+     
+     this.sortBy = snapshot.val().sortBy;
+     this.source = snapshot.val().source;
+     this.interval = snapshot.val().interval;
+    
+    });
+    
+  
+   
    }
 
   ngOnInit() {
-     /* setInterval(() => {
-       this.getNewsFeed();
-     }, 10000);*/
+   
+    setTimeout(() => {
+      this.getNewsFeed();
+      this.run();
+    },5000);
+  }
+
+  run():void{
+      setInterval(() => {
+      this.getNewsFeed();
+    },this.interval);
   }
 
   getNewsFeed(): void {
+  
+    this.API_URL = `https://newsapi.org/v1/articles?source=${this.source}&sortBy=${this.sortBy}&apiKey=${this.NEWS_API_KEY}`;
+    console.log("URl -->"+this.API_URL);
+
     this.http.get(this.API_URL).map(result => result.json()).subscribe(data => {
-      // console.log(data);
+      
+       console.log(data);
         this.newsArray = data.articles;
-        console.log(this.newsArray);
+      //  console.log(this.newsArray);
 
         let i = 0;
         this.title = this.newsArray[i].title;
@@ -68,14 +100,6 @@ export class NewsfeedComponent implements OnInit {
 
         }, 15000);
     });
-
-/*
-    for (let i = 0; i < 5; i++ ) {
-      this.title = this.newsArray[i].title;
-      this.description = this.newsArray[i].description;
-      this.author = this.newsArray[i].author;
-    }
-*/
   }
 
   reloadData(): void {
